f655df1a7b10877b9823b73ed5137a75d86eb1ce
Jemoy <jthrice1@gmail.com>
2022-04-01 20:20:03 -0500

    Fixed contacts sectiion mostly

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,21 @@ let links = document.getElementsByClassName("nav-links")[0];
 let nav = document.getElementsByTagName("nav")[0];
 function showNav(){
     if(links.style.display=="none") {
-        links.style.display = "flex"
+        links.style.display = "flex";
         links.style.opacity = "0";
         opacity = setInterval(decreaseOpacity, 30);
         
     }
     else {
-        links.style.display = "none"
+        links.style.display = "none";
     }
 
+}
+function hideNav(){
+
+    /*if(links.style.display!="none"){
+         links.style.display = "none";
+        }*/
 }
 function decreaseOpacity(){
     links.style.opacity= String(Number(links.style.opacity)+0.05);
