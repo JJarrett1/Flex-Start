66c2332ed54447b3e4d89d434d00b42d784da167
Jemoy <jthrice1@gmail.com>
2022-04-02 21:49:42 -0500

    Added features and wrapping up

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
 let links = document.getElementsByClassName("nav-links")[0];
 let nav = document.getElementsByTagName("nav")[0];
+
+let checkbox = document.getElementById("check");
+
 function showNav(){
-    if(links.style.display=="none") {
+    if(checkbox.checked == true) {
         links.style.display = "flex";
         links.style.opacity = "0";
-        opacity = setInterval(decreaseOpacity, 30);
-        
+        opacity = setInterval(decreaseOpacity, 30); 
     }
     else {
         links.style.display = "none";
     }
 
+console.log(links.style.display);
 }
 function hideNav(){
-
-    /*if(links.style.display!="none"){
-         links.style.display = "none";
-        }*/
+  checkbox.checked = false;
+  showNav();
 }
 function decreaseOpacity(){
     links.style.opacity= String(Number(links.style.opacity)+0.05);
