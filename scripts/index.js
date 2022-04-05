056271e8674262216d6c440d39fa38d00e8df816
Jemoy <jthrice1@gmail.com>
2022-04-04 23:01:39 -0500

    accordian (pending icons)

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,38 @@ let links = document.getElementsByClassName("nav-links")[0];
 let nav = document.getElementsByTagName("nav")[0];
 
 let checkbox = document.getElementById("check");
+let acc = document.getElementsByClassName("accordion");
+function showNav() {
+  if (checkbox.checked == true) {
+    links.style.display = "flex";
+    links.style.opacity = "0";
+    opacity = setInterval(decreaseOpacity, 30);
+  } else {
+    links.style.display = "none";
+  }
 
-function showNav(){
-    if(checkbox.checked == true) {
-        links.style.display = "flex";
-        links.style.opacity = "0";
-        opacity = setInterval(decreaseOpacity, 30); 
-    }
-    else {
-        links.style.display = "none";
-    }
-
-console.log(links.style.display);
+  console.log(links.style.display);
 }
-function hideNav(){
+function hideNav() {
   checkbox.checked = false;
   showNav();
 }
-function decreaseOpacity(){
-    links.style.opacity= String(Number(links.style.opacity)+0.05);
-    if(links.style.opacity == "1"){
-        clearInterval(opacity);
-    }
+function decreaseOpacity() {
+  links.style.opacity = String(Number(links.style.opacity) + 0.05);
+  if (links.style.opacity == "1") {
+    clearInterval(opacity);
+  }
 }
 
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
